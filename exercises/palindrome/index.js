6d64039989246fdd18915b642378e2a6e5d9c314
leannemcabey <leanne.m.cabey@gmail.com>
2019-04-02 16:43:52 -0400

    Complete palindrome exercise

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,9 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  let reversedString = str.split('').reduce((rev, char) => char + rev, '')
+  return str === reversedString
+}
 
 module.exports = palindrome;
