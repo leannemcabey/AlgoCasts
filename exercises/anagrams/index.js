41521fa23ac25281302a648b8ea424aba8490865
leannemcabey <leanne.m.cabey@gmail.com>
2019-05-05 16:07:26 -0400

    Complete anagrams exercise

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,50 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  // return stringA.toLowerCase().split('').sort().join() === stringB.toLowerCase().split('').sort().join()
+
+  let alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+  let stringACounts = {}
+  let stringBCounts = {}
+
+  for (let char of stringA) {
+    let lowerChar = char.toLowerCase()
+
+    if (alphabet.includes(lowerChar)) {
+      if (stringACounts[lowerChar]) {
+        stringACounts[lowerChar] = stringACounts[lowerChar]++
+      }
+      else {
+        stringACounts[lowerChar] = 1
+      }
+    }
+  }
+
+  for (let char of stringB) {
+    let lowerChar = char.toLowerCase()
+
+    if (alphabet.includes(lowerChar)) {
+      if (stringBCounts[lowerChar]) {
+        stringBCounts[lowerChar] = stringBCounts[lowerChar]++
+      }
+      else {
+        stringBCounts[lowerChar] = 1
+      }
+    }
+  }
+
+  if (Object.keys(stringACounts).length !== Object.keys(stringBCounts).length) {
+    return false
+  }
+
+  for (let char in stringACounts) {
+    if (stringACounts[char] !== stringBCounts[char]) {
+      return false
+    }
+  }
+
+  return true
+}
 
 module.exports = anagrams;
