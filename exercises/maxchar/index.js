1c709e3ce2db67a03b4af167902f8476fd372ad5
leannemcabey <leanne.m.cabey@gmail.com>
2019-05-05 14:07:10 -0400

    Complete maxchar exercise

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,28 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let charCounts = {}
+  let maxCount = 0
+  let maxCountChar = ''
+
+  str.split('').forEach(char => {
+    if (charCounts[char]) {
+      charCounts[char] = charCounts[char] + 1
+    }
+    else {
+      charCounts[char] = 1
+    }
+  })
+
+  for (let char in charCounts) {
+    if (charCounts[char] > maxCount) {
+      maxCount = charCounts[char]
+      maxCountChar = char
+    }
+  }
+
+  return maxCountChar
+}
 
 module.exports = maxChar;
