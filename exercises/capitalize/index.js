fdf77c0ef24a5785d62dcc9fd494c7984be3f323
leannemcabey <leanne.m.cabey@gmail.com>
2019-05-05 16:28:10 -0400

    Complete capitalize exercise

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,21 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  let splitString = str.split(' ')
+  let titleCasedArray = []
+
+  splitString.forEach(word => {
+    titleCasedArray.push(word[0].toUpperCase() + word.slice(1))
+
+    if (splitString.indexOf(word) !== splitString.length-1) {
+      titleCasedArray.push(' ')
+    }
+  })
+
+  return titleCasedArray.join('')
+
+  // Can also iterate through the characters and if the character before is a space than capitalize it and push
+}
 
 module.exports = capitalize;
